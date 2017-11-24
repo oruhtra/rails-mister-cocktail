2270f909a07baad388fb3af20840abe9d4dd31cd
arthur reboul <arthur.reboul@gmail.com>
2017-11-24 16:14:18 +0100

    picture reader

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,24 @@
-require 'open-uri'
-require 'json'
+# require 'open-uri'
+# require 'json'
 
-url = "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
-data_serialized = open(url).read
-data = JSON.parse(data_serialized)
-ingredients = data["drinks"]
+# url = "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+# data_serialized = open(url).read
+# data = JSON.parse(data_serialized)
+# ingredients = data["drinks"]
 
-i = 1
+# i = 1
 
-puts "destroy all ingrdients"
-Ingredient.delete_all
+# puts "destroy all ingrdients"
+# Ingredient.delete_all
 
-p ingredients
+# p ingredients
 
-puts "adding new ingredients"
-ingredients.each do |e|
-  puts "adding #{e["strIngredient1"]}"
-  Ingredient.create(name: e["strIngredient1"])
-  puts "created ingredient #{i}"
-  i += 1
-end
+# puts "adding new ingredients"
+# ingredients.each do |e|
+#   puts "adding #{e["strIngredient1"]}"
+#   Ingredient.create(name: e["strIngredient1"])
+#   puts "created ingredient #{i}"
+#   i += 1
+# end
 
-puts "all ingrdients added"
+# puts "all ingrdients added"
